e4e4a362233a45800b8f9939354ba07ba7b36296
sejyom <akxxkd01@gmail.com>
2023-12-06 19:32:29 +0900

    문제와 풀이

---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,9 @@
+package operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+//        `score` 가 80점 이상이고, 100점 이하이면 `true` 를 출력하고, 아니면 `false` 를 출력하세요.
+        int score = 80;
+        System.out.println(score>=80 && score<=100);
+    }
+}
