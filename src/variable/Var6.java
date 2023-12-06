b3e02e9140bf6702ebee2c5ccfe052ed1e796d8c
sejyom <akxxkd01@gmail.com>
2023-12-06 17:36:04 +0900

    변수 선언과 초기화

---
+++ src/variable/Var6.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a); //초기화 안 하면 컴파일 에러 발생
+
+        /*
+         * 컴퓨터 메모리는 여러 시스템이 함께 사용하기 때문에 어떠한 값들이 계속 저장되어 기존에 저장되어 있던 값이 무엇인지 모르는 상태이다.
+         * 따라서 초기화하지 않은 상태에서 출력하면 이상한 값이 출력될 수 있다.
+         * 이를 예방하기 위해 자바는 변수를 강제적으로 초기화 하도록 한다.
+         */
+
+    }
+}
