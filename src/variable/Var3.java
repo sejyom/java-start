7fb5db4171333d3c85558979910439142b90754e
sejyom <akxxkd01@gmail.com>
2023-12-06 17:23:21 +0900

    변수 값 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a;
+        a = 10; //변수 초기화(10)
+        System.out.println(a); //10
+        a = 50; //변수 값 변경(10->50)
+        System.out.println(a); //50
+    }
+}
