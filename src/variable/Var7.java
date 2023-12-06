297cb3b3a86b4f9643be5b47c1605d99c5546851
sejyom <akxxkd01@gmail.com>
2023-12-06 17:46:38 +0900

    변수 타입1

---
+++ src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수 리터럴
+        double b = 10.5; //실수 리터럴
+        boolean c = true; //불리언 리터럴 (true/false)
+        char d = 'A'; //캐릭터 리터럴, 문자 하나 ('')
+        String e = "Hello Java"; //문자열 리터럴 ("")
+
+        //개발자가 직접 입력한 고정 값을 리터럴이라고 함
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
