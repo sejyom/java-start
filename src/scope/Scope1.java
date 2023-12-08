4a70038cb1f13f3617c133a1bed738b817c74890
sejyom <akxxkd01@gmail.com>
2023-12-08 12:29:40 +0900

    스코프1 - 지역 변수와 스코프

---
+++ src/scope/Scope1.java
@@ -0,0 +1,14 @@
+package scope;
+
+public class Scope1 {
+    public static void main(String[] args) {
+        int n = 10; //n 생존 시작
+        if (true) {
+            int x = 20; //x 생존 시작
+            System.out.println("if n = " + n);
+            System.out.println("if x = " + x);
+        } //x 생존 종료
+        System.out.println("if n = " + n);
+//        System.out.println("if x = " + x);
+    } //n 생존 종료
+}
