50e3d6bbc364d55e598fe9bbfad4384c15ecfaf5
sejyom <akxxkd01@gmail.com>
2023-12-08 14:19:09 +0900

    형변환1 - 자동 형변환

---
+++ src/casting/Casting1.java
@@ -0,0 +1,21 @@
+package casting;
+
+public class Casting1 {
+    public static void main(String[] args) {
+        int intValue = 10;
+        long longValue;
+        double doubleValue;
+
+        //자동 형변환
+        longValue = intValue; //int -> long
+        System.out.println("longValue = " + longValue);
+
+        doubleValue = intValue; //int -> double
+        System.out.println("doubleValue = " + doubleValue);
+
+        doubleValue  = 2000000000L; //long -> double
+        System.out.println("doubleValue2 = " + doubleValue);
+
+
+    }
+}
